8419b7a677be7bb5159167b9af5feda8510ac3fd
RakeshKumarSidha <rakeshkumar.sidha@hcltech.com>
2025-01-27 15:51:27 +0530

    Update sample_api.py

--- sample_api.py
+++ sample_api.py
@@ -1,9 +1,9 @@
 def addtion_api(a,b):
-'''
-this api is used to add two numbers
-the results is retured
-args: a,b
-output: sum
-'''
-c = a + b
-return c
+  '''
+  this api is used to add two numbers
+  the results is retured
+  args: a,b
+  output: sum
+  '''
+  c = a + b
+  return c
